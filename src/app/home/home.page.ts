0fcc69d5cde05d0a11bf5d82eddd117546f5abf1
LePhew <marcos_eduardo16@hotmail.com>
2021-02-24 19:12:18 -0400

    Added infinite Scroll and Background image for card

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,5 +7,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() { }
+  readonly isDisabled: boolean = false;
+  readonly items = [];
+
+  constructor() {
+    this.pushData();
+  }
+
+
+  pushData() {
+    const max = this.items.length + 20;
+    const min = max - 20;
+    for (let i = min; i < max; i++) {
+      this.items.push(i);
+    }
+  }
+
+  loadData(ev: any) {
+    setTimeout(() => {
+      this.pushData();
+      console.log("Loaded data");
+      ev.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (this.items.length == 1000) {
+        ev.target.disabled = true;
+      }
+    }, 500);
+  }
+
 }
